2356afe6887383d18b150e26b3074b08567279d6
user <user@DESKTOP-OTOPGJN>
2022-09-18 19:22:17 +0300

    Ders6

---
+++ ders6/src/ders6/ders6.java
@@ -0,0 +1,17 @@
+package ders6;
+
+public class ders6 {
+
+	public static void main(String[] args) {
+		
+		int ogrenciSayisi=12;
+		String mesaj="öðrenci sayýsý: ";
+		System.out.println(mesaj + ogrenciSayisi);
+		System.out.println(mesaj + ogrenciSayisi);
+		System.out.println(mesaj + ogrenciSayisi);
+		System.out.println(mesaj + ogrenciSayisi);
+		System.out.println(mesaj + ogrenciSayisi);
+		
+	}
+
+}
