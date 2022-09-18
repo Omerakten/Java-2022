3c049d9f5c75fb23f2ff09184a0bef50c46bc0e3
user <user@DESKTOP-OTOPGJN>
2022-09-18 19:24:04 +0300

    Ders10

---
+++ ders10/src/ders10/ders10.java
@@ -0,0 +1,30 @@
+package ders10;
+
+public class ders10 {
+
+	public static void main(String[] args) {
+		char grade ='A';
+		
+		switch(grade) {
+		case 'A':
+			System.out.println("Mükemmel: Geçtiniz");
+		break;
+		case 'B':
+			System.out.println("Güzel : Geçtiniz");
+		break;
+		case 'C':
+			System.out.println("Fena deðil: Geçtiniz");
+		break;
+		case 'D':
+			System.out.println("Ýdare eder: Geçtiniz");
+		break;
+		case 'F':
+			System.out.println("Malesef : Kaldýnýz");
+		break;
+		
+		
+		}
+
+	}
+
+}
