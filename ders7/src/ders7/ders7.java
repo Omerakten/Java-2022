a7ff91f15582e7f001e77c07c15bec233a0d56b9
user <user@DESKTOP-OTOPGJN>
2022-09-18 19:23:14 +0300

    Ders7

---
+++ ders7/src/ders7/ders7.java
@@ -0,0 +1,26 @@
+package ders7;
+
+public class ders7 {
+
+	public static void main(String[] args) {
+		int sayi =12;
+		double sayi2=15.5;
+		byte sayi3=100;
+		short sayi4=30000;
+		long sayi5=500000000;
+		float sayi6=15.5f;
+		boolean dogru=false;
+		char karakter='E';
+		
+		System.out.println("Sayý1: "+sayi);
+		System.out.println("Sayý2: "+sayi2);
+		System.out.println("Sayý3: "+sayi3);
+		System.out.println("Sayý4: "+sayi4);
+		System.out.println("Sayý5: "+sayi5);
+		System.out.println("Sayý6: "+sayi6);
+		System.out.println("doðrumu: "+dogru);
+		System.out.println("karakter: "+karakter);
+		
+	}
+
+}
