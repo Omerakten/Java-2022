a267dd046c1c74e1804729fb3521ead40502c490
user <user@DESKTOP-OTOPGJN>
2022-09-18 19:18:22 +0300

    Ders8

---
+++ ders8/src/ders8/ders8.java
@@ -0,0 +1,20 @@
+package ders8;
+
+public class ders8 {
+
+	public static void main(String[] args) {
+		int sayi=24;
+		if(sayi<20) {
+			System.out.println("Sayı 20 den Küçük");
+		}
+		else if(sayi==20)
+		{
+			System.out.println("Sayı 20 den Küçük değildir");
+		}
+		else {
+			System.out.println("Sayı 20 den büyüktür");
+		}
+
+	}
+
+}
