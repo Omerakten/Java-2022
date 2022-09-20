2091cc73276210d456711ea475bdaa0348f59e90
user <user@DESKTOP-OTOPGJN>
2022-09-20 14:03:30 +0300

    ders11

---
+++ ders11/src/ders11/ders11.java
@@ -0,0 +1,15 @@
+package ders11;
+
+public class ders11 {
+
+	public static void main(String[] args) {
+		
+		for (int i = 1; i < 10; i+=2) {
+			
+			System.out.println(i);
+			
+		}
+		System.out.println("döngü bitti");
+	}
+
+}
