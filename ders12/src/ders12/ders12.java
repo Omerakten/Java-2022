a21c738c5477131dac846da0c80c9059d6f50087
user <user@DESKTOP-OTOPGJN>
2022-09-20 14:03:57 +0300

    ders12

---
+++ ders12/src/ders12/ders12.java
@@ -0,0 +1,28 @@
+package ders12;
+
+public class ders12 {
+
+	public static void main(String[] args) {
+		//for
+		for (int i = 2; i < 10; i+=2) {
+			
+			System.out.println(i);
+			
+		}
+		System.out.println("döngü bitti");
+	
+	// while
+	int i=2;
+	while(i<10) {
+		i+=2;
+		System.out.println(i);
+		
+	}
+	System.out.println("While döngüsü bitti");
+	
+	
+	
+	
+	
+	}
+}
